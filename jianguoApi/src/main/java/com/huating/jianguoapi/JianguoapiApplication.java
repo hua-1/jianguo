63b0abbd1cfe5223fa351365130cb8ac9d012419
lianghuating <lianghuating@zhaoguanche.com>
2018-07-02 16:55:35 +0800

    spring boot

---
+++ jianguoApi/src/main/java/com/huating/jianguoapi/JianguoapiApplication.java
@@ -0,0 +1,12 @@
+package com.huating.jianguoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JianguoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JianguoapiApplication.class, args);
+	}
+}
---
+++ jianguoApi/src/test/java/com/huating/jianguoapi/JianguoapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.huating.jianguoapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JianguoapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
